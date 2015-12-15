33231bcb90df126025593c501d1a46379ce9712c
Niels <Nozols0@gmail.com>
2015-12-15 14:03:42 +0100

    initial commit

---
+++ GuiTestBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GuiTest
+{
+    class GuiTestBehaviour : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Starting mod");
+            StartCoroutine(SpawnGuests());
+        }
+
+        private IEnumerator SpawnGuests()
+        {
+            for (;;)
+            {
+                Debug.Log("Spawning Guests");
+                GameController.Instance.park.spawnGuest();
+
+                yield return new UnityEngine.WaitForSeconds(1);
+            }
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace GuiTest
+{
+    class Main : IMod
+    {
+        private GameObject _go;
+
+        public string Description
+        {
+            get
+            {
+                return "Test for nice GUIs";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "GuiTest";
+            }
+        }
+
+        public void onDisabled()
+        {
+            Debug.Log("Disabling mod");
+            UnityEngine.Object.Destroy(_go);
+        }
+
+        public void onEnabled()
+        {
+            Debug.Log("Enabling mod");
+            _go = new GameObject();
+            _go.AddComponent<GuiTestBehaviour>();
+        }
+    }
+}
